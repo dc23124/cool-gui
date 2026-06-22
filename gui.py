58e7217ed228fbbbda94b763430e6323cf9fbae3
dc23124 <dc23124@mbc.school.nz>
2026-06-22 16:25:14 +1200

    Modify GUI title and layout in gui.py
    Updated GUI title and label text, added window size.

--- gui.py
+++ gui.py
@@ -1,15 +1,23 @@
 import tkinter as tk
+from tkinter import messagebox
 
-root = tk.Tk()
-root.title("Ticket Calculator")
+#  def submit_data(): 
+        
+#         name = input("input your name: ")   
+#         print("Invalid input. Please do not use numbers, spaces, or symbols.")
+#         receiptNumber = int(input("enter your receipt number: "))
+#         item = inp ut("enter the item that was hired: ")
+#         itemNum = input("enter the amount of items you hired: ") 
 
-label = tk.Label(root, text="This program calculates the prices for your tickets")
-label.pack(padx=30, pady=30)
-
-def on_click():
-    label.config(text="Click")
     
-button = tk.Button(root, text="Calculate", command=on_click)
-button.pack(padx=25, pady=12)
+#main window
+root = tk.Tk()
+root.title("Cool GUI")
+
+label = tk.Label(root, text="Hello World!")
+label.place
+
+# window size
+root.geometry("300x200")
 
 root.mainloop()
