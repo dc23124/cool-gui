ec8c586a7597e6dc140399e9bc3fd0978b51c5dc
dc23124 <dc23124@mbc.school.nz>
2026-06-28 12:14:01 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
+# strips out more than one space when user enters their inputs
 def get_input():
     name = entry_name.get().strip()
     receipt = entry_receipt.get().strip()
@@ -9,6 +10,18 @@ def get_input():
 
     if not name:
         messagebox.showwarning("Input Required", "Please enter your full name.")
+        return
+    
+    if not receipt.isdigit:
+        messagebox.showwarning("Input Required", "Please enter your full receipt.")
+        return
+    
+    if not item:
+        messagebox.showwarning("Input Required", "Please enter your full item.")
+        return
+    
+    if not amount or amount.isdigit:
+        messagebox.showwarning("Input Required", "Please enter your full amount.")
         return
 
     output = (
@@ -25,7 +38,7 @@ root = tk.Tk()
 root.title("Cool GUI")
 root.geometry("400x400")
 
-# labels + entry fields
+# labels and entry fields
 label_name = tk.Label(root, text="Full name required:")
 label_name.grid(padx=10, pady=5)
 entry_name = tk.Entry(root, width=30)
@@ -45,6 +58,16 @@ label_amount = tk.Label(root, text="Enter the amount of items:")
 label_amount.grid(padx=10, pady=5)
 entry_amount = tk.Entry(root, width=30)
 entry_amount.grid(padx=10, pady=5)
+
+def clear_entries():
+    entry_name.delete(0, tk.END)
+    entry_receipt.delete(0, tk.END)
+    entry_item.delete(0, tk.END)
+    entry_amount.delete(0, tk.END)
+
+# clear button
+clear_btn = tk.Button(root, text="Clear all", command=clear_entries)
+clear_btn.grid(padx=10, pady=5)
 
 # button to submit
 submit_btn = tk.Button(root, text="Submit", command=get_input)
